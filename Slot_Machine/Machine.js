ffa3e76bab5ef9f417a81a9c1f615d6ed84fd130
hellrambler <91743399+hellrambler@users.noreply.github.com>
2022-05-01 17:08:11 -0400

    styling

--- Slot_Machine/Machine.js
+++ Slot_Machine/Machine.js
@@ -3,13 +3,13 @@ class Machine extends React.Component {
     render() {
         console.log(this.props);
         return (
-            <div>
+            <div className="Machine">
                 <h4>
                     {(this.props.first === this.props.second && this.props.second === this.props.third)
                     ? "You win!!!!!" 
                     : "Sorry again"}
                 </h4>
-                <div>
+                <div style={{fontSize: '50px'}}>
                     <span>{this.props.first}</span>
                     <span>{this.props.second}</span>
                     <span>{this.props.third}</span>
